aeffa5a95b0ae5987eacde708df01f7ee4d81752
dpettas <dspedi@gmail.com>
2017-11-03 16:58:39 +0200

    I Created the Help Subroutine, for the easiest observation of the Class by the User

---
+++ src/methods/Read.f90
@@ -0,0 +1,19 @@
+
+
+
+  Subroutine     ReadStrUDIO (This, UNIT, IOTYPE, V_LIST, IOSTAT, IOMSG)
+
+    Class(Str)  , Intent(InOut)      :: This
+    Integer     , Intent(In)         :: UNIT
+    CHARACTER(*), Intent(In)         :: IOTYPE
+    INTEGER     , Intent(In)         :: V_LIST (:)
+    INTEGER     , Intent(Out)        :: IOSTAT
+    CHARACTER(*), Intent(InOut)      :: IOMSG
+
+    Character(len=10000)             :: Temp
+
+
+    Read (Unit, Fmt='(a)', Iostat=Iostat, Iomsg=Iomsg) Temp
+    This = Trim(Adjustl(Temp))
+  End Subroutine ReadStrUDIO
+   
